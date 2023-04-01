fa02086cc47dc69d75e03051c1bc42cf842e27a0
DESKTOP-HGRP1DQ\leon <szymonwojcik800@gmail.com>
2023-04-01 08:53:37 +0200

    Register dbcontext

--- src/WarehouseManagment.Infrastructure/InfrastructureModule.cs
+++ src/WarehouseManagment.Infrastructure/InfrastructureModule.cs
@@ -1,13 +1,16 @@
 ﻿
 
 using Autofac;
+using Microsoft.EntityFrameworkCore;
+using WarehouseManagment.Infrastructure.Data;
 
 namespace WarehouseManagment.Infrastructure
 {
     public sealed class InfrastructureModule : Module
     {
         protected override void Load(ContainerBuilder builder)
-        {
+        {  
+
             builder.RegisterAssemblyTypes(typeof(IInfrastructureAssemblyMarker).Assembly)
                 .AsImplementedInterfaces();
         }
--- src/WarehouseManagmentApi/Program.cs
+++ src/WarehouseManagmentApi/Program.cs
@@ -1,15 +1,21 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 using WarehouseManagment.Api;
+using WarehouseManagment.Infrastructure.Data;
 using WarehouseManagment.Web;
 
 var builder = WebApplication.CreateBuilder(args);
-
+IConfiguration configuration = builder.Configuration;
 
 // Call UseServiceProviderFactory on the Host sub property 
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory(x => x.RegisterModule<WebApiModule>()));
 
 // Add services to the container.
+builder.Services.AddDbContext<WarehouseContext>(options =>
+    options.UseNpgsql(configuration.GetConnectionString("WarehouseDatabase")));
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
