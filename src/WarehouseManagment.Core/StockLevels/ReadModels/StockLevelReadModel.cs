00966dea63699c0ea36cf203e4a48302e5d24242
Szymon <szymonwojcik800@gmail.com>
2023-06-18 06:38:52 +0200

    Add read model for stock level

---
+++ src/WarehouseManagment.Core/StockLevels/ReadModels/StockLevelReadModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WarehouseManagment.Core.StockLevels.ReadModels
+{
+    public sealed class StockLevelReadModel
+    {
+        public long StockLevelId { get; set; }
+        public long ProductsInStock { get; set; }
+        public long ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductManufacturer { get; set; }
+    }
+}
